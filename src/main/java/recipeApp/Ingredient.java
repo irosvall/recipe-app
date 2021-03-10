e345a1be23a8a4a4dc8f67dd723d596fc6dc51bf
Ida Rosvall <ir222gn@student.lnu.se>
2021-03-10 14:47:57 +0100

    Create ingredient class

--- src/main/java/recipeApp/Ingredient.java
+++ src/main/java/recipeApp/Ingredient.java
@@ -1,5 +1,30 @@
 package recipeApp;
 
 public class Ingredient {
-  
+  private String name;
+  private String unit;
+  private Double price;
+
+  Ingredient(String name, String unit, Double price) {
+    this.name = name;
+    this.unit = unit;
+    this.price = price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
+
+  @Override
+  public String toString() {
+    return "Ingredient: " + name + ", unit: " + unit + ", price: " + price;
+  }
 }
