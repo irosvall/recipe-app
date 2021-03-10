985054f796d65338be436882a5e98fc715d25dd8
Ida Rosvall <ir222gn@student.lnu.se>
2021-03-10 16:16:41 +0100

    Create recipe class

--- src/main/java/recipeApp/Recipe.java
+++ src/main/java/recipeApp/Recipe.java
@@ -1,5 +1,33 @@
 package recipeApp;
 
+import java.util.ArrayList;
+
 public class Recipe {
-  
+  private String name;
+  private int portions;
+  private String instructions;
+  private ArrayList<RecipeIngredient> ingredients;
+
+  Recipe(String name, int portions, String instructions) {
+    this.name = name;
+    this.portions = portions;
+    this.instructions = instructions;
+    ingredients = new ArrayList<>();
+  }
+
+  public ArrayList<RecipeIngredient> getIngredients() {
+    return ingredients;
+  }
+
+  public String getInstructions() {
+    return instructions;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getPortions() {
+    return portions;
+  }
 }
