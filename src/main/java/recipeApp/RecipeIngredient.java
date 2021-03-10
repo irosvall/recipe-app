08e75272c0d90ba589512262ea22d5cc7cc6bc06
Ida Rosvall <ir222gn@student.lnu.se>
2021-03-10 16:54:51 +0100

    Create the recipe ingredient class

--- src/main/java/recipeApp/RecipeIngredient.java
+++ src/main/java/recipeApp/RecipeIngredient.java
@@ -1,5 +1,25 @@
 package recipeApp;
 
 public class RecipeIngredient {
-  
+  private Ingredient ingredient;
+  private Double amount;
+  private String comments;
+
+  RecipeIngredient(Ingredient ingredient, Double amount, String comments) {
+    this.ingredient = ingredient;
+    this.amount = amount;
+    this.comments = comments;
+  }
+
+  public Double getAmount() {
+    return amount;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public Ingredient getIngredient() {
+    return ingredient;
+  }
 }
